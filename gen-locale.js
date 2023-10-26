6059edf8823058002e0ba0317832975221305847
luongduy2798 <nhatduy2727@gmail.com>
2023-10-26 13:53:15 +0700

    gen key

--- gen-locale.js
+++ gen-locale.js
@@ -1,13 +1,20 @@
-import { translate } from '@vitalets/google-translate-api';
-import { HttpProxyAgent } from 'http-proxy-agent';
 import { TEXT } from "./file-init/text.js";
 import fs from 'fs';
+import { Translate } from '@google-cloud/translate/build/src/v2/index.js';
+import * as serviceAccount from './service-account.json' assert { type: "json" };
+const LANG_KEY = 'en';
 
 function removeDuplicates(arr) {
   return [...new Set(arr)];
 }
+const service = new Translate({ projectId: 'myhome-e4c53', credentials: serviceAccount.default });
 
 const listText = removeDuplicates(TEXT.split('\n')).filter((item) => item != '')
-const gen = listText.reduce((a, v) => ({ ...a, [v]: v }), {})
+const gen = {}
+await Promise.all(listText.map(async (item, index) => {
+  const [text] = await service.translate(item, LANG_KEY)
+  const newText = text.charAt(0).toUpperCase() + text.slice(1);
+  gen[newText] = item
+}))
 fs.writeFile(`gen/gen.json`, JSON.stringify(gen, null, 2), (err) => { });
 

568a419db1c68e404f71ad14fcaf8fde244bcdd3
luongduy2798 <nhatduy2727@gmail.com>
2023-12-21 10:00:47 +0700

    fixbug

--- gen-locale.js
+++ gen-locale.js
@@ -2,7 +2,7 @@ import { TEXT } from "./file-init/text.js";
 import fs from 'fs';
 import { Translate } from '@google-cloud/translate/build/src/v2/index.js';
 import * as serviceAccount from './service-account.json' assert { type: "json" };
-const LANG_KEY = 'en';
+const LANG_KEY = '';
 
 function removeDuplicates(arr) {
   return [...new Set(arr)];
@@ -12,7 +12,7 @@ const service = new Translate({ projectId: 'myhome-e4c53', credentials: serviceA
 const listText = removeDuplicates(TEXT.split('\n')).filter((item) => item != '')
 const gen = {}
 await Promise.all(listText.map(async (item, index) => {
-  const [text] = LANG_KEY.length > 0 ? await service.translate(item, LANG_KEY) : [item]
+  const [text] = LANG_KEY.length > 0 ? await service.translate(item, LANG_KEY) : [item.trim()]
   const newText = text.charAt(0).toUpperCase() + text.slice(1);
   gen[newText] = item
 }))
